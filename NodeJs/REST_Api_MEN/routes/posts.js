db48316a0a4dab25120921b17e2f68f0c46c6c5e
eslam <esla.ahme@hotmail.com>
2020-08-27 18:34:07 +0200

    all is working

--- NodeJs/REST_Api_MEN/routes/posts.js
+++ NodeJs/REST_Api_MEN/routes/posts.js
@@ -16,6 +16,17 @@ router.get('/',(req,res)=>{
     });
 });
 
+router.get('/:id',(req,res)=>{
+    
+    const post = Post.findOne({_id:req.params.id});
+    post.exec()
+    .then(post =>{
+        res.send(post)
+    }).catch(err=>{
+        res.status(404).send({message:err});
+    });
+})
+
 router.get('/',(req,res)=>{
     const posts = Post.find().limit(5);
     posts.exec()
@@ -26,10 +37,7 @@ router.get('/',(req,res)=>{
     });
 });
 
-
-
 router.post('/',(req,res)=>{
-
     const p = new Post({
         title: req.body.title,
         description : req.body.description
@@ -43,6 +51,28 @@ router.post('/',(req,res)=>{
     })
     
 });
+router.delete('/:id',(req,res)=>{
+    Post.remove({_id:req.params.id})
+    .exec()
+    .then(result => {
+           res.send(result);
+    })
+    .catch(err => {
+        res.status(404).send({message:err});
+    });
+})
+
+router.patch('/:id',(req,res)=>{
+
+    Post.updateOne({ _id: req.params.id },{ title : req.body.title, description : req.body.description})
+    .exec()
+    .then(result => {
+        res.send(result);  
+    })
+    .catch(err => {
+        res.status(404).send({message:err});
+    });
+})
 
 
 module.exports = router;
